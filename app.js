76760385197085169c6a742b532c86915cdf0779
chrisdfe <christopher.d.ferris@gmail.com>
2016-03-03 16:45:21 -0800

    added endpoint

--- app.js
+++ app.js
@@ -6,10 +6,25 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var nunjucks = require('express-nunjucks');
 
 var routes = require('./routes/index');
 
 var app = express();
+
+
+var app = express();
+
+app.set('view engine', 'nunjucks');
+
+// Templates in this directory will override the application templates.
+app.set('views', __dirname + '/views');
+
+// Configuring the template system.
+nunjucks.setup({
+  autoescape: true,
+  watch: true
+}, app);
 
 var env = process.env.NODE_ENV || 'development';
 app.locals.ENV = env;
@@ -27,9 +42,9 @@ app.use('/', routes);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 /// error handlers
@@ -38,26 +53,26 @@ app.use(function(req, res, next) {
 // will print stacktrace
 
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
 
-        res.render('error', {
-            message: err.message,
-            error: err,
-            title: 'error'
-        });
+    res.render('error', {
+      message: err.message,
+      error: err,
+      title: 'error'
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {},
-        title: 'error'
-    });
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {},
+    title: 'error'
+  });
 });
 
 
--- routes/index.coffee
+++ routes/index.coffee
@@ -1,9 +1,17 @@
+fs = require 'fs'
+path = require 'path'
 express = require 'express'
 utils = require '../utils'
 
 router = express.Router()
 
 router.get '/', (req, res) ->
-  res.render 'index', { title: 'Express' }
+  res.json 'go to /random_zombie_joke'
+
+router.get '/random_zombie_joke', (req, res) ->
+  jokes = fs.readFileSync path.resolve './data/jokes.json'
+  jokes = JSON.parse jokes
+  index = Math.floor Math.random() * jokes.length
+  res.json jokes[index]
 
 module.exports = router
